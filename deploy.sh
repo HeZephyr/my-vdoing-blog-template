25dbf1e27b757598890c1e9544b15d6fd31ff3bb
zfhe <2825841950@qq.com>
2023-11-21 18:58:05 +0800

    refactor:修改项目结构，重置deploy方式

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,28 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-
-push_addr=git@github.com:unique-pure/unique-pure.github.io.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-commit_info=`git describe --all --always --long`
-dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=main # 推送的分支
+# 推送博客
+git add .
+git commit -m $1
+git push
 
 # 生成静态文件
 npm run build
 
+# 推送github pages
+# push_address=git@github.com:unique-pure/unique-pure.github.io.git # git提交地址
+commit_info=`git describe --all --always --long`
+dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
+push_branch=main # 推送的分支
+
 # 进入生成的文件夹
 cd $dist_path
+echo 'unique-pure.github.io' > CNAME
 
-git init
 git add -A
 git commit -m "deploy: $commit_info"
-git push -f $push_addr HEAD:$push_branch
+git push
 
 cd -
-rm -rf $dist_path
 
 
